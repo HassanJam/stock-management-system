b340d031c977c18a8d1611aeddff0a97a695151a
Hassan Jamshaid <hassanjamshaid364@gmail.com>
2024-11-04 11:56:17 +0500

    api added error resolved

--- src/services/authService.js
+++ src/services/authService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import auth_api from '../api/api.js'
 
-const API_URL = `${stocks_api}`;
+const API_URL = `${auth_api}`;
 
 export const register = async (username, password, department) => {
     return await axios.post(`${API_URL}/register`, { username, password, department });
